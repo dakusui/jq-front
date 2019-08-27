9ca6246f4d30ef4e52d169bbbadb926add47c8be
Hiroshi Ukai <dakusui@gmail.com>
2019-08-28 06:15:46 +0900

    Initial commit

---
+++ powerjson.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -eu
+
+function all_path() {
+  local _json="${1}"
+  echo "${_json}" | jq -r -c 'path(..)|[.[]]|map(if type=="number" then "["+tostring+"]" else "\""+tostring+"\"" end)|join(".")|gsub("\\.\\[";"[")|"."+tostring'
+}
+
+function value_at() {
+  local _path="${1}"
+  local _json="${2}"
+  echo "${_json}" | jq "${_path}"
+}
+
+
+function expand_extends_keywords() {
+  local _target="${1}"
+  local _cur="$(mktemp)"
+  echo '{}' > "${_cur}"
+  for i in $(all_path "$(cat ${_target})" | grep '"$extends"$'); do
+    local _next="$(mktemp)"
+    for j in $(jq -r -c "$i[]" "${_target}"); do
+      jq -n "input | ${i%.\"\$extends\"}=input" "${_cur}" "${j}" > "${_next}"
+      _cur="${_next}"
+    done;
+  done
+  jq -r -c . "${_cur}"
+}
+
+
+function remove_meta_nodes() {
+  local _target="${1}"
+  local _cur="$(mktemp)"
+  cp "${_target}" "${_cur}"
+  for i in $(all_path "$(cat "${_target}")" | grep '"$extends"$'); do
+    local _next="$(mktemp)"
+    jq ".|del(${i})" "${_cur}" > "${_next}"
+    _cur="${_next}"
+  done
+  cat "${_cur}"
+}
+
+function merge_object_nodes() {
+  local _a="${1}"
+  local _b="${2}"
+  jq -s '.[0] * .[1]' "${_a}" "${_b}"
+}
+
+
+function expand_json() {
+  local _target="${1}"
+  local _extends="$(mktemp)"
+  local _clean="$(mktemp)"
+
+  expand_extends_keywords "${_target}" > "${_extends}"
+  remove_meta_nodes "${_target}" > "${_clean}"
+  merge_object_nodes "${_clean}" "${_extends}"
+}
