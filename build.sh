b1f6ca1c5d0ccd56e605431c34daaaa8920e6d84
Hiroshi Ukai <dakusui@gmail.com>
2019-09-01 04:47:47 +0900

    Separate lenient and strict schemata.

--- build.sh
+++ build.sh
@@ -4,15 +4,19 @@ set -eu
 echo "DOCUMENT:"
 docker run -it \
        --user 1000:1000 \
-       -v $(pwd):/documents/ \
+       -v "$(pwd)":/documents/ \
        asciidoctor/docker-asciidoctor \
        asciidoctor -r asciidoctor-diagram README.adoc -o docs/index.html
+echo "...DONE"
+echo ""
 
 echo "TESTING:"
 bash -eu "tests/tests.sh" "$(pwd)/jf" "$(pwd)/tests"
-
+echo "...DONE"
 echo ""
 
 echo "BUILDING DOCKER IMAGE:"
 docker build -t dakusui/jf .
+echo "...DONE"
+echo ""
 
