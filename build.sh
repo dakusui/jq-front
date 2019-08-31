80a4fef0371a4fcc2da059c2c71290267202df6c
Hiroshi Ukai <dakusui@gmail.com>
2019-08-31 20:29:34 +0900

    Rename README.html to index.html

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 # Run tests
 set -eu
 
+echo "DOCUMENT:"
+docker run -it \
+       --user 1000:1000 \
+       -v $(pwd):/documents/ \
+       asciidoctor/docker-asciidoctor \
+       asciidoctor -r asciidoctor-diagram README.adoc -o docs/index.html
+
 echo "TESTING:"
 bash -eu "tests/tests.sh" "$(pwd)/jf" "$(pwd)/tests"
 
