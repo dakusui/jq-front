91db4f5f2110d6b7ee28c20ddcb8bdcecd4bb8df
Hiroshi Ukai <dakusui@gmail.com>
2019-09-01 08:50:41 +0900

    Improve test framework

--- tests/tests.sh
+++ tests/tests.sh
@@ -6,25 +6,34 @@ _TEST_ROOT_DIR="${2:?Specify a directory in which test dirs are stored.}"
 JF_PATH=.:${_TEST_ROOT_DIR}/base
 export JF_PATH
 
-function runtest() {
-  local _dirname="${1%/input.json}"
+function run_normal_test() {
+  local _dirname="${1}"
   local _diff
   local _ret=1
-  echo -n "executing test:'${_dirname}':..."
   ${_JF} "${_dirname}/input.json" > "${_dirname}/test-output".json || return 1
-  diff <(jq -S . "${_dirname}/expected.json") <(jq -S . "${_dirname}/test-output.json") >\
-   "${_dirname}/test-output.diff" || return 1
-  _diff=$(cat "${_dirname}/test-output.diff")
+  {
+    diff <(jq -S . "${_dirname}/expected.json") <(jq -S . "${_dirname}/test-output.json") > "${_dirname}/test-output.diff"
+   }
+  _diff=$(cat "${_dirname}/test-output.diff") || return 1
   if [[ -z ${_diff} ]]; then
-    echo "PASSED" >&2
+    echo "PASSED" 1>&2
     _ret=0
   else
     _ret=1
     echo "FAILED: diff--->
     ${_diff}
-    ---" >&2
+    ---" 1>&2
   fi
   return ${_ret}
+}
+
+
+function runtest() {
+  local _dirname="${1}"
+  local _test_type
+  _test_type="$(jq -r -c ".testType" "${_dirname}/test.json")" || return 1
+  echo -n "executing test(${_test_type}):'${_dirname#${_TEST_ROOT_DIR}/}':..."
+  "run_${_test_type}_test" "${_dirname}" || return 1
 }
 
 function clean() {
@@ -37,12 +46,9 @@ function main() {
   echo "${_TEST_ROOT_DIR}" 1>&2
   while IFS= read -r -d '' i
   do
-    i="${i%/input.json}"
-    if [[ ${i} == *base || -f "${i}" ]]; then
-      continue
-    fi
-    runtest "${i}" || failed=$((${failed} + 1))
-  done <   <(find "${_TEST_ROOT_DIR}" -type f -name input.json -print0)
+    i="${i%/test.json}"
+    runtest "${i}" || failed=$((failed + 1))
+  done <   <(find "${_TEST_ROOT_DIR}" -type f -name test.json -print0)
 
   if [[ $failed == 0 ]]; then
     echo "all tests passed"
