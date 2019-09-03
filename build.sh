0fcba7fbec3065412ea805b9bce7146370bd800c
Hiroshi Ukai <dakusui@users.noreply.github.com>
2019-09-04 05:42:56 +0900

    Issue 23 implement validation (#24)
    Issue-#23: 
    * Add schemata (stript and lenient version)
    * Update documentaion and Dockerfile for validation support
    * Improve build.sh
    * Improve test framework
    * Integrate validation logic
    * Fix a broken test and script.
    * Fix bugs in inheritance mechanism
    * Add a test for inter-private-nodal-inheritances
    * Uniform test names
    * Improve private node directory handling
    * Compact error messages
    * Fix a bug in resolving input file name
    * Uniform use of 'true'/'false' and 'T'/'F' to the former returned by shared functions
    * Clean up temporary file generation
    * Simplify expression to list all paths in a json object
    * Remove too much debug logs
    * Rename abort to quit
    * Separate quit and abort
    * clean up variable names in _expand_internal_inheritances
    * Implement plugin feature

--- build.sh
+++ build.sh
@@ -1,18 +1,54 @@
 # Run tests
 set -eu
 
-echo "DOCUMENT:"
-docker run -it \
-       --user 1000:1000 \
-       -v $(pwd):/documents/ \
-       asciidoctor/docker-asciidoctor \
-       asciidoctor -r asciidoctor-diagram README.adoc -o docs/index.html
+function message() {
+  local _message="${1}"
+  echo "${_message}" 1>&2
+}
 
-echo "TESTING:"
-bash -eu "tests/tests.sh" "$(pwd)/jf" "$(pwd)/tests"
+function execute_doc() {
+  docker run -it \
+         --user 1000:1000 \
+         -v "$(pwd)":/documents/ \
+         asciidoctor/docker-asciidoctor \
+         asciidoctor -r asciidoctor-diagram README.adoc -o docs/index.html
+}
 
-echo ""
+function execute_package() {
+  docker build -t dakusui/jf .
+}
 
-echo "BUILDING DOCKER IMAGE:"
-docker build -t dakusui/jf .
+function execute_test() {
+  local _target_tests="${1:-*}"
+  bash -eu "tests/tests.sh" "$(pwd)/jf" "$(pwd)/tests" "${_target_tests}"
+}
 
+function execute_release() {
+  docker login
+}
+
+function execute_stage() {
+  local _stage="${1}"
+  shift
+  message "EXECUTING:${_stage}..."
+  {
+    "execute_${_stage}" "${@}" && message "DONE:${_stage}"
+  } || {
+    message "FAILED:${_stage}"
+    return 1
+  }
+  return 0
+}
+
+function main() {
+  if [[ $# == 0 ]]; then
+    main doc test package
+  fi
+  for i in "$@"; do
+    local _args
+    IFS=':' read -r -a _args <<<"${i}"
+    execute_stage "${_args[@]}" || exit 1
+  done
+}
+
+main "${@}"
--- tests/tests.sh
+++ tests/tests.sh
@@ -1,28 +1,102 @@
 #!/bin/bash -eu
+set -eu
 
 _JF="${1:?Specify 'jf' to test in absolute path}"
 _TEST_ROOT_DIR="${2:?Specify a directory in which test dirs are stored.}"
+_TARGET_TESTS="${3:-*}"
 JF_PATH=.:${_TEST_ROOT_DIR}/base
 export JF_PATH
 
-function runtest() {
-  local _dirname="${1}"
+function run_normal_test() {
+  local _testfile="${1}"
+  local _dirname
   local _diff
-  local _ret=0
-  echo -n "executing test:'${_dirname}':..."
-  ${_JF} "${_dirname}/input.json" > "${_dirname}/test-output".json
-  diff <(jq -S . "${_dirname}/expected.json") <(jq -S . "${_dirname}/test-output.json") >\
-   "${_dirname}/test-output.diff"
-  _diff=$(cat "${_dirname}/test-output.diff")
+  local _ret=1
+  _dirname="$(dirname ${_testfile})"
+  ${_JF} "${_dirname}/input.json" > "${_dirname}/test-output".json || return 1
+  {
+    diff <(jq -S . "${_dirname}/expected.json") <(jq -S . "${_dirname}/test-output.json") > "${_dirname}/test-output.diff"
+   }
+  _diff=$(cat "${_dirname}/test-output.diff") || return 1
   if [[ -z ${_diff} ]]; then
-    echo "PASSED" >&2
+    echo "PASSED" 1>&2
+    _ret=0
   else
     _ret=1
     echo "FAILED: diff--->
     ${_diff}
-    ---" >&2
+    ---" 1>&2
   fi
   return ${_ret}
+}
+
+function _failure_message() {
+  local _expected_validity_level="${1}"
+  local _schema="${2}"
+  local _expectation="${3}"
+  echo "(Expectation:${_expected_validity_level}):Expected to be ${_expectation} with ${_schema} schema but not:'${_dirname/input.txt}'" >&2
+  cat "${_dirname}/test-output-${_schema}".txt >&2
+}
+
+function run_validation_test() {
+  local _testfile="${1}"
+  local _actual_strict
+  local _actual_lenient
+  local _expected_validity_level
+  local _ret=0
+  _expected_validity_level=$(jq -r -c ".expectation.validityLevel" "${_testfile}")
+  _dirname="$(dirname ${_testfile})"
+  ${_JF} "${_dirname}/input.json" -d --validation=lenient &> "${_dirname}/test-output-lenient".txt
+  _actual_lenient=$?
+  ${_JF} "${_dirname}/input.json" -d --validation=strict &> "${_dirname}/test-output-strict".txt
+  _actual_strict=$?
+  if [[ "${_expected_validity_level}" == "strict" ]]; then
+    if [[ "${_actual_lenient}" != 0 ]]; then
+      _failure_message "${_expected_validity_level}" "lenient" "valid"
+      _ret=1
+    fi
+    if [[ "${_actual_strict}" != 0  ]]; then
+      _failure_message "${_expected_validity_level}" "strict" "valid"
+      _ret=1
+    fi
+  elif [[ "${_expected_validity_level}" == "lenient" ]]; then
+    if [[ "${_actual_lenient}" != 0 ]]; then
+      _failure_message "${_expected_validity_level}" "lenient" "valid"
+      _ret=1
+    fi
+    if [[ "${_actual_strict}" == 0  ]]; then
+      _failure_message "${_expected_validity_level}" "strict" "INVALID"
+      _ret=1
+    fi
+  elif [[ "${_expected_validity_level}" == "invalid" ]]; then
+    if [[ "${_actual_lenient}" == 0 ]]; then
+      _failure_message "${_expected_validity_level}" "lenient" "INVALID"
+      _ret=1
+    fi
+    if [[ "${_actual_strict}" == 0  ]]; then
+      _failure_message "${_expected_validity_level}" "strict" "INVALID"
+      _ret=1
+    fi
+  else
+    echo "Unknown expectation validity level:'${_expected_validity_level}' was specified." 1>&2
+    return 1
+  fi
+  if [[ ${_ret} == 0 ]]; then
+    echo "PASSED" 1>&2
+  else
+    echo "FAILED" 1>&2
+  fi
+  return ${_ret}
+}
+
+
+function runtest() {
+  local _test_json="${1}"
+  local _dirname="${_test_json%/test.json}"
+  local _test_type
+  _test_type="$(jq -r -c ".testType" "${_test_json}")" || return 1
+  echo -n "executing test(${_test_type}):'${_dirname#${_TEST_ROOT_DIR}/}':..."
+  "run_${_test_type}_test" "${_test_json}" || return 1
 }
 
 function clean() {
@@ -30,22 +104,28 @@ function clean() {
 }
 
 function main() {
-  local failed
-  failed=0
-  for i in "${_TEST_ROOT_DIR}"/*; do
-    if [[ ${i} == *base || -f "${i}" ]]; then
-      continue
+  local failed=0
+  local skipped=0
+  local numtests=0
+  echo "${_TEST_ROOT_DIR}" 1>&2
+  while IFS= read -r -d '' i
+  do
+    numtests=$((numtests + 1))
+    if [[ "x${i}" == x${_TARGET_TESTS} ]]; then
+      runtest "${i}" || failed=$((failed + 1))
+    else
+      skipped=$((skipped + 1))
+      echo "Skipping ${i}"
     fi
-    runtest "${i}" || failed=$((${failed} + 1))
-  done
+  done <   <(find "${_TEST_ROOT_DIR}" -type f -name test.json -print0)
 
   if [[ $failed == 0 ]]; then
-    echo "all tests passed"
+    echo "No test failed (total=${numtests}; skipped=${skipped})"
   else
-    echo "$failed test(s) FAILED"
+    echo "$failed test(s) FAILED (total=${numtests}; skipped=${skipped})"
     return 1
   fi
 }
 
-clean
+clean || exit 1
 main || exit 1
