a9dee0cd719fc4e7bab672737c51853105a82f0a
Hiroshi Ukai <dakusui@gmail.com>
2019-09-09 07:38:06 +0900

    WIP: local pool inheritance

--- sandbox.sh
+++ sandbox.sh
@@ -4,16 +4,9 @@ set -E -eu
 function func1() {
   cat "${1}"
 }
-function fname() {
-  if [[ "${1}" == "missing" ]]; then
-    return 1
-  fi
-  echo "$1"
-}
-declare var
-export  -f func1
-export  -f fname
-var=$(func1 "${1}")
-echo "var=${var}"
 
-echo "finished"
+declare -a arr
+arr=(HELLO WORLD)
+
+func2 "hello" "${arr[@]}" asdf
+
