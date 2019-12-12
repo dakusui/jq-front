2fb2f43be11e75107b47ca68dee2d6d3563e5419
Hiroshi Ukai <dakusui@gmail.com>
2019-12-13 00:23:50 +0900

    Issue #113: Correct version name shown by docker version of jq-front

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM ubuntu:18.04
+ARG VERSION=unknown
 RUN apt-get update && \
     apt-get install jq -y && \
     apt-get install npm -y && \
@@ -8,8 +9,10 @@ RUN apt-get update && \
     npm install -g ajv-cli && \
     mkdir -p /var/lib/jq-front && \
     mkdir -p /app/lib && \
-    mkdir -p /app/schema
+    mkdir -p /app/schema && \
+    echo $VERSION > /app/version_file
 COPY ./jq-front /app
+COPY ./build_info.sh /app
 COPY ./build_info.json /app
 COPY ./lib /app/lib
 COPY ./schema /app/schema
--- build_info.sh
+++ build_info.sh
@@ -1,9 +1,10 @@
-LATEST_RELEASED_VERSION="$(printf "v%s.%s" "$(jq -r -c '.version.latestReleased.major' build_info.json)" \
-                                           "$(jq -r -c '.version.latestReleased.minor' build_info.json)")"
-TARGET_VERSION="$(printf "v%s.%s" "$(jq -r -c '.version.target.major' build_info.json)" \
-                                  "$(jq -r -c '.version.target.minor' build_info.json)")"
-APP_NAME="$(jq -r -c '.appName' build_info.json)"
-DOCKER_USER_NAME="$(jq -r -c '.docker.user' build_info.json)"
+BASEDIR="${JF_BASEDIR:-"."}"
+LATEST_RELEASED_VERSION="$(printf "v%s.%s" "$(jq -r -c '.version.latestReleased.major' "${BASEDIR}/build_info.json")" \
+                                           "$(jq -r -c '.version.latestReleased.minor' "${BASEDIR}/build_info.json")")"
+TARGET_VERSION="$(printf "v%s.%s" "$(jq -r -c '.version.target.major' "${BASEDIR}/build_info.json")" \
+                                  "$(jq -r -c '.version.target.minor' "${BASEDIR}/build_info.json")")"
+APP_NAME="$(jq -r -c '.appName' "${BASEDIR}/build_info.json")"
+DOCKER_USER_NAME="$(jq -r -c '.docker.user' "${BASEDIR}/build_info.json")"
 export LATEST_RELEASED_VERSION
 export TARGET_VERSION
 export APP_NAME
