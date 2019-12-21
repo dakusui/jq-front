8e70c53783be21350c46288f65419291f32317b2
Hiroshi Ukai <dakusui@gmail.com>
2019-12-21 09:10:48 +0900

    Clean up file structure Update document (remove abolished environmant variables)

--- lib/helpers.sh
+++ lib/helpers.sh
@@ -1,8 +1,13 @@
 [[ "${_HELPERS_SH:-""}" == "yes" ]] && return 0
 _HELPERS_SH=yes
 
-function _debug_abort() {
-  print_stacktrace "DEBUG: error trapped"
-  exit 1
+function print_global_variables() {
+  debug "_JF_CWD=${_JF_CWD}"
+  ####
+  # JF_PATH_BASE is set when this program is run under Docker.
+  debug "_JF_PATH_BASE=${_JF_PATH_BASE}"
+  debug "_JF_PATH=${_JF_PATH}"
+  debug "_JF_INFO=${_JF_INFO}"
+  debug "_JF_DEBUG=${_JF_DEBUG}"
+  debug "_JF_PERF=${_JF_PERF}"
 }
-
